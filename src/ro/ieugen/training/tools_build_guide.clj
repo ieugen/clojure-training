37dd8695486876cf612d62e1a449588c6fa9fd5a
Eugen Stan <eugen.stan@netdava.com>
2021-08-26 22:29:08 +0300

    tools.build guide

---
+++ src/ro/ieugen/training/tools_build_guide.clj
@@ -0,0 +1,54 @@
+(ns ro.ieugen.training.tools-build-guide
+  (:require [clojure.tools.build.api :as b]))
+
+
+(def lib 'my/lib)
+(def version (format "1.2.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+
+(defn prep [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir}))
+
+(defn uber [_]
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis}))
+
+
+(comment
+
+  (jar nil)
+  
+  (uber nil)
+
+  (println "Help")
+  
+  0
+  )
