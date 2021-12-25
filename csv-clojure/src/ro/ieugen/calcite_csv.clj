4a252252c574ebf59181f3918cd8bc8565815a81
Eugen Stan <eugen.stan@netdava.com>
2021-12-25 23:01:33 +0200

    Schema factory works ok, schema implementation fails

--- csv-clojure/src/ro/ieugen/calcite_csv.clj
+++ csv-clojure/src/ro/ieugen/calcite_csv.clj
@@ -3,27 +3,38 @@
             [clojure.string :as str]
             [next.jdbc :as jdbc])
   (:import (java.io File)
-           (java.util HashSet
+           (java.util ArrayList
+                      HashSet
                       HashMap
+                      Map
                       Objects)
+           (org.apache.calcite DataContext$Variable)
            (org.apache.calcite.adapter.file CsvEnumerator
                                             JsonScannableTable)
-           (org.apache.calcite.model ModelHandler
-                                     ModelHandler$ExtraOperand)
-           (org.apache.calcite.rel.type RelProtoDataType)
+           (org.apache.calcite.linq4j AbstractEnumerable)
+           (org.apache.calcite.model ModelHandler$ExtraOperand)
+           (org.apache.calcite.rel.type RelDataTypeFactory
+                                        RelProtoDataType)
            (org.apache.calcite.schema ScannableTable
                                       Schema
                                       Schema$TableType
                                       SchemaPlus
                                       Schemas
-                                      SchemaFactory
                                       Statistics
                                       Table
                                       Wrapper)
+           (org.apache.calcite.sql SqlNode)
+           (org.apache.calcite.sql.parser SqlParser)
            (org.apache.calcite.util Source
-                                    Sources)))
+                                    Sources
+                                    ImmutableIntList)))
 
 (def csv-table-flavors [:scannable :filterable :translatable])
+
+(defn get-field-types
+  [^RelDataTypeFactory type-factory source field-types is-stream ]
+  ;; TODO: In java this is cached.
+  (CsvEnumerator/deduceRowType type-factory source field-types is-stream))
 
 (defn scannable-table
   ^Table [^Source source ^RelProtoDataType proto-row-type]
@@ -40,7 +51,18 @@
           (nil? proto-row-type) (.apply proto-row-type type-factory)
           (nil? @row-type) (do
                              (reset! row-type (CsvEnumerator/deduceRowType type-factory source nil (-> this (.isStream))))
-                             @row-type))))))
+                             @row-type)))
+      (toString [this] "ro.ieugen.calcite-csv/scannable-table")
+      (scan [this root]
+        (let [type-factory (.getTypeFactory root)
+              field-types (get-field-types type-factory source (ArrayList.) (.isStream this))
+              fields (ImmutableIntList/identity (.size field-types))
+              cancel-flag (.get DataContext$Variable/CANCEL_FLAG root)
+              row-converter (CsvEnumerator/arrayConverter field-types fields false)]
+          (proxy
+           [AbstractEnumerable] []
+            (enumerator [this]
+              (CsvEnumerator. source cancel-flag false nil row-converter))))))))
 
 (defn translatable-table
   ^Table [^Source source proto-row-type]
@@ -106,11 +128,28 @@
         ;; empty map - no files / tables found
         {}))))
 
+(defn schema-directory-file
+  "Return path for schema directory.
+   If path is not absolute, prepend base."
+  ^File [^File base ^String directory]
+  (let [directory-file (io/file directory)
+        nil-base? (nil? base)
+        not-absolute-directory? (not (.isAbsolute directory-file))]
+    (if (and nil-base? not-absolute-directory?)
+      (io/file base directory)
+      directory-file)))
 
-(defn csv-schema [dir flavor]
-  (let [dir dir
-        flavor flavor
-        tables (create-table-map dir flavor)
+(defn csv-schema
+  "Create a Calcite Schema and return it for use."
+  ^Schema [^SchemaPlus parent-schema ^String name ^Map operand]
+  (println "csv-schema" parent-schema "name:" name "operand:" operand)
+  (let [directory (.get operand "directory")
+        baseDirName (.camelName ModelHandler$ExtraOperand/BASE_DIRECTORY)
+        base (io/file (.get operand baseDirName))
+        directory-file (schema-directory-file base directory)
+        flavor-name (or (.get operand "flavor") "scannable")
+        flavor (keyword (str/lower-case flavor-name))
+        tables (create-table-map directory-file flavor)
         types (atom {})
         sub-schemas (atom {})]
     (reify
@@ -123,38 +162,10 @@
       (getTableNames [this] (.keySet tables))
       (getTable [this name] (.get tables name))
       (getType [this name] (.get tables name))
-      (getFunctions [this name] (throw (UnsupportedOperationException. "Not implemented")))
-      (getFunctionNames [this] (throw (UnsupportedOperationException. "Not implemented")))
-      (getSubSchemaNames [this] (HashSet. (keys @sub-schemas)))
+      (getFunctions [this name] (HashSet.))
+      (getFunctionNames [this] (HashSet.))
+      (getSubSchemaNames [this] (HashSet.))
       (getSubSchema [this name] (get @sub-schemas name)))))
-
-(defn schema-directory-file
-  "Return path for schema directory.
-   If path is not absolute, prepend base."
-  ^File [^File base ^String directory]
-  (let [directory-file (io/file directory)
-        nil-base? (nil? base)
-        not-absolute-directory? (not (.isAbsolute directory-file))]
-    (if (and nil-base? not-absolute-directory?)
-      (io/file base directory)
-      directory-file)))
-
-(defn csv-schema-factory
-  ^SchemaFactory []
-  (reify SchemaFactory
-    (create [this parent-schema name operand]
-      (let [directory (.get operand "directory")
-            baseDirName (.camelName ModelHandler$ExtraOperand/BASE_DIRECTORY)
-            base (io/file (.get operand baseDirName))
-            directory-file (schema-directory-file base directory)
-            flavor-name (or (.get operand "flavor") "scannable")
-            flavor (keyword (str/lower-case flavor-name))]
-        (csv-schema directory-file flavor)))))
-
-
-(defn- main [& args]
-  (println "Hello"))
-
 
 (comment
 
@@ -168,19 +179,22 @@
   (let [x ["a" "b" "c"]]
     (into {} (map (fn [f] [f (str "val-" f)]) x)))
 
-  (create-table-map (io/file "../csv/src/test/resources/sales") :scannable)
+  (create-table-map (io/file "resources/sales") :scannable)
 
-  (let [schema-factory (csv-schema-factory)
-        operand (doto (HashMap.)
-                  (.put "directory" "../csv/src/test/resources/sales")
-                  (.put "flavor" "scannable"))]
-    (.create schema-factory nil "SALES" operand))
-
+  (require '[clojure.tools.trace :as trace])
+  (trace/trace-ns 'ro.ieugen.calcite-csv)
 
   (let [db {:jdbcUrl "jdbc:calcite:model=resources/model.json"
             :user "admin"
             :password "admin"}
         ds (jdbc/get-datasource db)]
-    (jdbc/execute! ds ["select * from sales"]))
+    (jdbc/execute! ds ["select * from SALES"]))
 
+  (let [operand (doto (HashMap.)
+                  (.put "directory" "resources/sales")
+                  (.put "flavor" "scannable"))
+        query "select * from sales"
+        ^SqlParser parser (SqlParser/create query)
+        ^SqlNode sql-node (.parseQuery parser)]
+    (println (.toString sql-node)))
   )
